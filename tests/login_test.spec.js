d19d7551917777e01bbef9e51fea7cfbe6ee0595
anngarciaa <annabellagarcia03@gmail.com>
2024-10-01 12:52:38 +0800

    added a file

--- tests/login_test.spec.js
+++ tests/login_test.spec.js
@@ -3,8 +3,22 @@ import { test, expect } from '@playwright/test';
 test('Login test', async ({ page }) => {
 
     await page.goto('https://sandbox.yourfreedomprojecthub.com/');
-    await expect(page).toHaveTitle('When you sell, you create resistance, when you educate, you build rapport and trust.');
-    await page.pause();
-    await page.locator('input[name="Auth[email]"').fill('group-devs@virtual-wonders.com');
-    await page.locator('input[name="Auth[password]"').fill('demo');
+    await page.getByText('Sign In').click();
+
+    await page.getByLabel('email').fill('group-devs@virtual-wonders.com', {delay:200});
+    await page.getByLabel('password').fill('demo', {delay:100});
+
+    await page.locator("//button[@type='submit']").click();
+
+    // await page.waitForTimeout(5000);
+
+    await expect(page).toHaveURL(/dashboard/);
+
+    await page.getByText("AT").first().click();
+    await page.getByText("Logout").click();
+
+    // await page.waitForTimeout(3000);
+
+    await expect(page).toHaveURL('https://sandbox.yourfreedomprojecthub.com/');
 });
+
